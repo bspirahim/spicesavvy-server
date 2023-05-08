ee5fb9fb13a8ca219e29495a37b8ebc1b985c788
Abdur-P-Rahim <bspirahim@gmail.com>
2023-05-09 01:04:01 +0600

    food, chef, category done

--- index.js
+++ index.js
@@ -4,21 +4,16 @@ var cors = require('cors')
 const port = process.env.PORT || 5000
 
 const chef = require('./data/chef.json')
-const recipe = require('./data/recipe.json')
 const category = require('./data/category.json')
 const food = require('./data/food.json')
 app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('food hub running')
+  res.send('Spicesavvy running')
 })
 
 app.get('/chef', (req, res) => {
   res.send(chef)
-})
-
-app.get('/recipe', (req, res) => {
-  res.send(recipe)
 })
 
 
@@ -33,39 +28,39 @@ app.get('/food', (req, res) => {
 
 
 
-app.get('/chef/:id', (req, res) =>{
-    const id = req.params.id;
-    console.log(id)
-    const selectedChef = chef.find(c => c.id == id);
-    res.send(selectedChef)
+app.get('/chef/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(id)
+  const selectedChef = chef.find(c => c.id == id);
+  res.send(selectedChef)
 })
 
-/* app.get('/food/:id', (req, res) =>{
+app.get('/chef-food/:id', (req, res) => {
   const id = parseInt(req.params.id);
   console.log(id)
-  if(id === 0){
+  if (id === 0) {
     res.send(food)
   }
-  else{
+  else {
     const chefRecipe = food.filter(n => parseInt(n.chef_id) === id);
     res.send(chefRecipe)
   }
-}) */
+})
 
-app.get('/category/:id', (req, res) =>{
+app.get('/category/:id', (req, res) => {
   const id = req.params.id;
   console.log(id)
   const selectedCategory = category.find(cg => cg.id == id);
   res.send(selectedCategory)
 })
 
-app.get('/food/:id', (req, res) =>{
+app.get('/category-food/:id', (req, res) => {
   const id = parseInt(req.params.id);
   console.log(id)
-  if(id === 0){
+  if (id === 0) {
     res.send(food)
   }
-  else{
+  else {
     const categoryFood = food.filter(f => parseInt(f.category_id) === id);
     res.send(categoryFood)
   }
@@ -73,5 +68,5 @@ app.get('/food/:id', (req, res) =>{
 
 
 app.listen(port, () => {
-  console.log(`Food hub api is running out ${port}`)
+  console.log(`Spicesavvy api is running out ${port}`)
 })
